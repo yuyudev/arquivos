34dc13ecb6d29abdb5c04376f106d9caeabc9ca5
Yuri Christian <yurichristiann@gmail.com>
2022-06-12 23:42:00 -0300

    Atualizacao: 12/06/2022

--- apis/robo/programinha.py
+++ apis/robo/programinha.py
@@ -1,13 +1,23 @@
 from tkinter import *
 from tkinter import ttk
-from tkinter import filedialog
+from tkinter.filedialog import askopenfilename
 import pandas as pd
+from codecs import open as codeopen
 
+#Upload do arquivo
+
+def wrap_text_file():    
+    file = codeopen(
+        askopenfilename(filetypes=[('CSV Files', '*.csv')]), 
+        encoding='utf-8')
+    yield file
+    file.close()
+
+#Interface
 
 janela = Tk()
 janela.title("Tratamento dos dados de ASO")
 janela.geometry("500x300")
-
 
 listaClientes = ["BanQi", "Privalia"]
 
@@ -21,29 +31,24 @@ cb_clientes.pack()
 banqi = cb_clientes.set("BanQi")
 privalia = cb_clientes.set("Privalia")
 
-
 lb_periodo = Label(janela, text="Selecione o período dos dados:")
 lb_periodo.pack()
-
-
 
 lb_inicio = Label(janela, text="Início: ")
 lb_inicio.pack()
 dataInicial = Entry(janela, width=10)
 dataInicial.pack()
 
-
-
 lb_final = Label(janela, text="Final: ")
 lb_final.pack()
 dataFinal = Entry(janela, width=10)
 dataFinal.pack()
 
-      #  "2022-06-07":"2022-05-08"
-    
+#Funções de tratamento
+
 def tratar_banqi():
-    arquivoPasta = filedialog.askopenfile()
-    arquivo = pd.read_csv(arquivoPasta, encoding = "UTF-8", sep = ",")
+    arquivoPasta = next(wrap_text_file())
+    arquivo = pd.read_csv(arquivoPasta, sep = ",")
     datas = arquivo.T[str(dataFinal.get()):str(dataInicial.get())]
     termos = arquivo["Term"]
     posicoes = arquivo.T[str(dataFinal.get()):str(dataInicial.get())]
@@ -71,7 +76,7 @@ def tratar_banqi():
 
     for data in datas:
         for termo in termos:      
-            dados.append((data,termo,chave[i]), encodings="UTF-8")
+            dados.append((data,termo,chave[i]))
             i = i + 1
         
 
@@ -79,8 +84,8 @@ def tratar_banqi():
     base.to_csv("C:/Users/yuric/Documents/github/apis/robo/nova_base.csv")
 
 def tratar_privalia():
-    arquivoPasta = filedialog.askopenfile()
-    arquivo = pd.read_csv(arquivoPasta, encoding = "UTF-8", sep = ",")
+    arquivoPasta = next(wrap_text_file())
+    arquivo = pd.read_csv(arquivoPasta, encoding = "utf8", sep = ",")
     datas = arquivo.T[str(dataFinal.get()):str(dataInicial.get())]
     termos = arquivo["Term"]
     appId = arquivo["App ID"].values
@@ -95,7 +100,6 @@ def tratar_privalia():
     colunaTermos = column = termos
     colunaAppID = column = appId
     colunaDatas = column = datas
-
 
     #tratamento dos dados
 
@@ -114,11 +118,9 @@ def tratar_privalia():
             j = j + 1
             i = i + 1
         j=0
-        
-
 
     base = pd.DataFrame(dados, columns = ["datas", "termos", "id", "posição"])
-    base.to_csv("C:/Users/yuric/Documents/github/apis/robo/nova_base.csv", encoding="UTF-8")
+    base.to_csv("C:/Users/yuric/Documents/github/apis/robo/nova_base.csv")
 
 def tratar_dados():
     cliente = cb_clientes.get()
@@ -127,9 +129,7 @@ def tratar_dados():
     elif cliente == "Privalia":
         tratar_privalia()
 
-
 botao = Button(janela, text="Go!", width=5, height=1, background="lightblue", command=tratar_dados)
 botao.pack()
-
 
 janela.mainloop()
